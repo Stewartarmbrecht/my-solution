449f08cd82c40a4d7bf307d1bd3af3e7eea4376d
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-10 16:56:42 +0000

    Fixing headless init, pull, and push.

---
+++ libs/my-backend/amplify-init.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const shell = require('shelljs');
+const configPL = require('./amplify-configure-payload');
+const configCmd =
+  'amplify init --amplify "' +
+  JSON.stringify(configPL.amplify).replaceAll('"', '\\"') +
+  '" --frontend "' +
+  JSON.stringify(configPL.frontend).replaceAll('"', '\\"') +
+  '" --providers "' +
+  JSON.stringify(configPL.providers).replaceAll('"', '\\"') +
+  '" --yes';
+  
+if (shell.exec(configCmd).code !== 0) {
+  shell.echo('Error amplify configure failed.');
+  shell.exit(1);
+}
