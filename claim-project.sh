817cc406b4be6eb73b0265ee21723977cbda522d
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-14 12:35:48 +0000

    fixing claim-project

--- claim-project.sh
+++ claim-project.sh
@@ -4,5 +4,8 @@ noCaseChange=$(echo $1)
 lowercaseSedCommand="s/my/$lowercase/"
 noCaseChangeSedCommand="s/My/$noCaseChange/"
 for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+echo "Files with my in the name have been renamed to $1"
 for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+echo "Files with my in the name have been renamed to $1"
 for file in $(git ls-files | grep My | sed -e 's/\(My[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
+echo "Files with My in the name have been renamed to $1"
