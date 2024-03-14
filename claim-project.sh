db4da9236eff7bb11d2cff44fe0e2c3ec887a9f2
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-14 12:33:25 +0000

    Fixing claim-project

--- claim-project.sh
+++ claim-project.sh
@@ -4,4 +4,5 @@ noCaseChange=$(echo $1)
 lowercaseSedCommand="s/my/$lowercase/"
 noCaseChangeSedCommand="s/My/$noCaseChange/"
 for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
 for file in $(git ls-files | grep My | sed -e 's/\(My[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
