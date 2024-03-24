b148d6761b48de847e1929afe367c0a6531557e4
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-24 13:17:52 +0000

    Fixing file rename bugs.

--- tools/scripts/claim-project.sh
+++ tools/scripts/claim-project.sh
@@ -5,18 +5,20 @@ lowercaseSedCommand="s/my/$lowercase/"
 noCaseChangeSedCommand="s/My/$noCaseChange/"
 for file in $(git ls-files | grep my | uniq); 
 do 
-  dir=$(dirname $file);
-  mkdir -p $dir;
-  git mv $file $(echo $file | sed -e $lowercaseSedCommand);
+  newFile=$(echo $file | sed -e $lowercaseSedCommand)
+  dir=$(dirname $newFile)
+  mkdir -p $dir
+  git mv $file $newFile
 done
 
 echo "Files with my in the name have been renamed to $lowercase"
 
 for file in $(git ls-files | grep My | uniq); 
 do 
-  dir=$(dirname $file);
-  mkdir -p $dir;
-  git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); 
+  newFile=$(echo $file | sed -e $noCaseChangeSedCommand)
+  dir=$(dirname $newFile)
+  mkdir -p $dir
+  git mv $file $newFile
 done
 
 echo "Files with My in the name have been renamed to $1"
