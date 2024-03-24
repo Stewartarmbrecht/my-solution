df88aca94066872d6a252dc5509e1f37c91c46cc
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-24 12:54:30 +0000

    Improving claim-project file path updates.

--- tools/scripts/claim-project.sh
+++ tools/scripts/claim-project.sh
@@ -7,5 +7,15 @@ for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do 
 echo "Files with my in the name have been renamed to $1"
 for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
 echo "Files with my in the name have been renamed to $1"
+for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+echo "Files with my in the name have been renamed to $1"
+for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+echo "Files with my in the name have been renamed to $1"
+for file in $(git ls-files | grep my | sed -e 's/\(my[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+echo "Files with my in the name have been renamed to $1"
+for file in $(git ls-files | grep My | sed -e 's/\(My[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
+echo "Files with My in the name have been renamed to $1"
+for file in $(git ls-files | grep My | sed -e 's/\(My[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
+echo "Files with My in the name have been renamed to $1"
 for file in $(git ls-files | grep My | sed -e 's/\(My[^/]*\).*/\1/' | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
 echo "Files with My in the name have been renamed to $1"
