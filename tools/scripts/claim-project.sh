70c280fe13535e6a10d0615dfa89c30b0cfa92e5
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-03-24 13:14:36 +0000

    Fixing file rename bugs.

--- tools/scripts/claim-project.sh
+++ tools/scripts/claim-project.sh
@@ -3,19 +3,20 @@ lowercase=$(echo $1 | tr '[:upper:]' '[:lower:]')
 noCaseChange=$(echo $1)
 lowercaseSedCommand="s/my/$lowercase/"
 noCaseChangeSedCommand="s/My/$noCaseChange/"
-for file in $(git ls-files | grep my | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
+for file in $(git ls-files | grep my | uniq); 
+do 
+  dir=$(dirname $file);
+  mkdir -p $dir;
+  git mv $file $(echo $file | sed -e $lowercaseSedCommand);
+done
+
 echo "Files with my in the name have been renamed to $lowercase"
-for file in $(git ls-files | grep my | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
-echo "Files with my in the name have been renamed to $lowercase"
-for file in $(git ls-files | grep my | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
-echo "Files with my in the name have been renamed to $lowercase"
-for file in $(git ls-files | grep my | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
-echo "Files with my in the name have been renamed to $lowercase"
-for file in $(git ls-files | grep my | uniq); do git mv $file $(echo $file | sed -e $lowercaseSedCommand); done
-echo "Files with my in the name have been renamed to $1"
-for file in $(git ls-files | grep My | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
-echo "Files with My in the name have been renamed to $1"
-for file in $(git ls-files | grep My | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
-echo "Files with My in the name have been renamed to $1"
-for file in $(git ls-files | grep My | uniq); do git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); done
+
+for file in $(git ls-files | grep My | uniq); 
+do 
+  dir=$(dirname $file);
+  mkdir -p $dir;
+  git mv $file $(echo $file | sed -e $noCaseChangeSedCommand); 
+done
+
 echo "Files with My in the name have been renamed to $1"
