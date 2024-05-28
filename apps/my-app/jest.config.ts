3a7793a6997d1bd3045086631fef82a786d5c985
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2024-05-28 11:47:34 +0000

    Unit Tests passing. Still working on detox tests.

--- apps/my-app/jest.config.ts
+++ apps/my-app/jest.config.ts
@@ -6,7 +6,11 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-redux)',
   ],
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['./test-setup.tsx', '@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    './test-setup.tsx', 
+    '@testing-library/jest-native/extend-expect',
+    '../../node_modules/react-native-gesture-handler/jestSetup.js'
+  ],
   moduleNameMapper: {
     '\\.svg$': '@nx/expo/plugins/jest/svg-mock',
   },
